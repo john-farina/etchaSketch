596369d2bb6c427f9782f05afb21923d56c6e212
onefootfarina <clichecontent@gmail.com>
2022-02-27 21:21:19 -0500

    added clicking color; will add drag maybe later

--- etchaSketch.js
+++ etchaSketch.js
@@ -6,19 +6,18 @@ rowContainer.style.display = 'flex';
 rowContainer.style.gap = '2px';
 container.appendChild(rowContainer);
 
-function createRow(itemNum) {
+function createRow(boxNum) {
     //CREATES A DIV TO PUT THE BOXES IN
     const rowContainer = document.createElement('div');
-    rowContainer.classList.add('rowCont');
+    rowContainer.classList.add('rowContainer');
     rowContainer.style.display = 'flex';
+    // rowContainer.style.gap = '2px';
+    // rowContainer.style.marginBottom = '2px';
     container.appendChild(rowContainer);
-    //LOOP TO
-    let sqaureCount = 1;
-    while (sqaureCount <= itemNum) {
-        sqaureCount++;
+    //LOOP TO ITEMNUMBER, CREATES A BOX
+    for (i = 1; i <= boxNum; i++) {
         const contentOne = document.createElement('div');
-        contentOne.classList.add('square');
-        contentOne.style.backgroundColor = 'red';
+        contentOne.classList.add('grid');
         contentOne.style.borderStyle = 'solid';
         contentOne.style.width = '20px';
         contentOne.style.height = '20px';
@@ -31,3 +30,11 @@ while (c <= 16) {
     c++;
     createRow(16);
 }
+
+let divs = document.querySelectorAll('.grid');
+const changeBackgroundColor = (e) => {
+    e.target.style.backgroundColor = 'black';
+};
+divs.forEach((div) => {
+    div.addEventListener('click', changeBackgroundColor, { once: true });
+});
