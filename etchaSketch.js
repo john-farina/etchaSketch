55b4ef8d6428e9687d3d69ad66164e0c4fbd5364
onefootfarina <clichecontent@gmail.com>
2022-02-27 20:54:29 -0500

    made the 16x16 grid. finally!

--- etchaSketch.js
+++ etchaSketch.js
@@ -1,13 +1,33 @@
 const container = document.querySelector('#main-content');
-container.style.display = 'flex';
-let sqaureCount = 1;
 
-while (sqaureCount <= 16) {
-    sqaureCount++;
-    const content = document.createElement('div');
-    content.classList.add('colorDiv');
-    content.style.flex = '1';
-    content.style.backgroundColor = 'red';
-    content.textContent = '1';
-    container.appendChild(content);
+const rowContainer = document.createElement('div');
+rowContainer.classList.add('rowCont');
+rowContainer.style.display = 'flex';
+rowContainer.style.gap = '2px';
+container.appendChild(rowContainer);
+
+function createRow(itemNum) {
+    //CREATES A DIV TO PUT THE BOXES IN
+    const rowContainer = document.createElement('div');
+    rowContainer.classList.add('rowCont');
+    rowContainer.style.display = 'flex';
+    container.appendChild(rowContainer);
+    //LOOP TO
+    let sqaureCount = 1;
+    while (sqaureCount <= itemNum) {
+        sqaureCount++;
+        const contentOne = document.createElement('div');
+        contentOne.classList.add('square');
+        contentOne.style.backgroundColor = 'red';
+        contentOne.style.borderStyle = 'solid';
+        contentOne.style.width = '20px';
+        contentOne.style.height = '20px';
+        rowContainer.appendChild(contentOne);
+    }
+}
+
+let c = 1;
+while (c <= 16) {
+    c++;
+    createRow(16);
 }
