4a733c28c72463164bb68c495f1ee6c18884158e
onefootfarina <clichecontent@gmail.com>
2022-03-01 00:34:40 -0500

    added reset button, added colors. added resizing code for more boxes, going to add change grid function soon

--- etchaSketch.js
+++ etchaSketch.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#main-content');
-
 const rowContainer = document.createElement('div');
+let userChoice = 60;
+
 rowContainer.classList.add('rowCont');
 rowContainer.style.display = 'flex';
 rowContainer.style.gap = '2px';
@@ -11,19 +12,41 @@ function createRow(boxNum) {
     const rowContainer = document.createElement('div');
     rowContainer.classList.add('rowContainer');
     rowContainer.style.display = 'flex';
-    // rowContainer.style.gap = '2px';
-    // rowContainer.style.marginBottom = '2px';
     container.appendChild(rowContainer);
     //LOOP TO ITEMNUMBER, CREATES A BOX
     for (i = 1; i <= boxNum; i++) {
         const contentOne = document.createElement('div');
         contentOne.classList.add('grid');
-        contentOne.style.borderStyle = 'dotted';
-        contentOne.style.width = '20px';
-        contentOne.style.height = '20px';
+        contentOne.style.borderStyle = 'solid';
+        contentOne.style.borderWidth = '.06rem';
+        contentOne.style.backgroundColor = 'white';
+
+        //VARIABLE TO RESET BACKGROUND COLOR
+        const resetBackgroundColor = (e) => {
+            e.target.style.backgroundColor = 'white';
+        };
+        //FINDS RESET BUTTON
+        const reset = document.querySelector('.reset');
+        //LISTENS FOR CLICK ON BUTTON THEN CHANGES IT TO WHITE
+        reset.addEventListener('click', () => {
+            contentOne.style.backgroundColor = 'white';
+        });
+
+        if (boxNum <= 23) {
+            contentOne.style.width = '20px';
+            contentOne.style.height = '20px';
+        } else if (boxNum > 23 && boxNum <= 40) {
+            contentOne.style.width = '10px';
+            contentOne.style.height = '10px';
+        } else if (boxNum > 40 && boxNum <= 70) {
+            contentOne.style.width = '6px';
+            contentOne.style.height = '6px';
+        }
+
         rowContainer.appendChild(contentOne);
     }
 }
+
 function createGrid(rowNum, columnNum) {
     let c = 1;
     while (c <= columnNum) {
@@ -32,12 +55,22 @@ function createGrid(rowNum, columnNum) {
     }
 }
 
-createGrid(16, 16);
+const changeGridSize = document.querySelector('.change-size');
+changeGridSize.addEventListener('click', () => {
+    let userChoice = prompt('Choose a # 1-70');
+    console.log(userChoice);
+});
 
-let divs = document.querySelectorAll('.grid');
+createGrid(userChoice, userChoice);
+
+//FINDS ALL DIVS WITH GRID CLASS
+const divs = document.querySelectorAll('.grid');
+//CHANGE BACKGROUND EVENT VARIABLE
 const changeBackgroundColor = (e) => {
     e.target.style.backgroundColor = 'black';
 };
+//EACH DIV ADDS AN EVENT LISTEN FOR THE MOUSE ENTERING,THEN CHANGE
+//BACKGROUND
 divs.forEach((div) => {
-    div.addEventListener('click', changeBackgroundColor, { once: true });
+    div.addEventListener('mouseenter', changeBackgroundColor);
 });
