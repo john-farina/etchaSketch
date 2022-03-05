e2dd45e8eba56ef4b49a337437f21c1bc462880e
onefootfarina <clichecontent@gmail.com>
2022-03-04 19:19:25 -0500

    fixed the code!!!!!!! thank the lord. broken last column tho, not sure why

--- etchaSketch.js
+++ etchaSketch.js
@@ -50,7 +50,27 @@ function createRow(boxNum) {
     // gridContainer.appendChild(rowContainer);
     //LOOP TO ITEMNUMBER, CREATES A BOX
     for (i = 1; i <= boxNum; i++) {
-        let square = createSquare(boxNum);
+        // //FINDS RESET BUTTON
+        const reset = document.querySelector('.reset');
+        //LISTENS FOR CLICK ON BUTTON THEN CHANGES IT TO WHITE
+        reset.addEventListener('click', () => {
+            square.style.backgroundColor = 'white';
+        });
+        let square = document.createElement('div');
+        square.classList.add('grid');
+        square.style.borderStyle = 'solid';
+        square.style.borderWidth = '.06rem';
+        square.style.backgroundColor = 'white';
+        if (boxNum <= 23) {
+            square.style.width = '20px';
+            square.style.height = '20px';
+        } else if (boxNum > 23 && boxNum <= 40) {
+            square.style.width = '10px';
+            square.style.height = '10px';
+        } else if (boxNum > 40 && boxNum <= 70) {
+            square.style.width = '6px';
+            square.style.height = '6px';
+        }
         rowContainer.appendChild(square);
     }
     addPaintListen();
